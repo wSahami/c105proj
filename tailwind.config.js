4b19c7181b1a95cc57d873299f4499a26d3632a1
wSahami <wosahami@gmail.com>
2024-12-09 13:01:39 -0500

    Biblio update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: [
     './index.html',
+    './success.html',
+    './callingin.html',
+    './callingon.html',
+    './callingout.html',
+    './biblio.html',
     './Quizzes/quiz1.html'
   ],
   theme: {
